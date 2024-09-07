f02ed3bc66b8a401bee51b81c241f5be067a86b0
marik611377 <109631968+marik611377@users.noreply.github.com>
2024-09-07 18:50:01 +1000

    Updated Script

--- combatinitation.lua
+++ combatinitation.lua
@@ -159,4 +159,15 @@ Tab3:AddButton({
 		game.Players.LocalPlayer.Character:SetAttribute("UtilityBoost", OrionLib.Flags["utilityboostvalue"].Value)
   	end    
 })
+local Tab4 = Window:MakeTab({
+	Name = "Info",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab4:AddParagraph("v2.1.1","Updated for")
+Tab4:AddParagraph("v1","Current version")
+Tab4:AddParagraph("marik611277","Made by")
+Tab4:AddParagraph("Synapse X Remake","Created firstly in")
+Tab4:AddParagraph("Non-GUI script","Inspired by")
+Tab4:AddParagraph("discord.gg/HmP63uHfWq","Synapse X Remake Discord:")
 OrionLib:Init()
