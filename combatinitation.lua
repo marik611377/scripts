39da68d823f7d37e30c1c485b66f7f87d60ea27b
marik611377 <109631968+marik611377@users.noreply.github.com>
2024-10-31 14:20:12 +1000

    Update combatinitation.lua

--- combatinitation.lua
+++ combatinitation.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Combat Initation Script (v2.1.2)", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "Combat Initation Script (v2.1.2)", HidePremium = false, SaveConfig = true, ConfigFolder = "CombatInitationConfig"})
 
 -- Function to modify tool attributes safely (check both Backpack and equipped tools)
 local function modifyToolAttributes(toolName, attributes)
