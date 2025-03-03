fe572c2848cd2ed09fb9169ed7219ec0b2ad59d5
marik611377 <109631968+marik611377@users.noreply.github.com>
2025-03-03 14:49:16 +1000

    Update notifier.lua

--- notifier.lua
+++ notifier.lua
@@ -1,18 +1,21 @@
 local gui = Instance.new("ScreenGui")
 gui.Parent = game:WaitForChild("CoreGui")
+gui.Name = "Notifier"
 
 local ts = game:GetService("TweenService")
 local ti = TweenInfo.new(0.7)
 
 local notifier = {}
 
-function notifier.new(text: string, duration: number, color: Color3)
+function notifier.new(text: string, duration: number, color: Color3, textcolor: Color3)
     local notify = Instance.new("TextLabel")
     text = text or "nil"
     duration = duration or 3
     color = color or Color3.new(1, 1, 1)
+    textcolor = textcolor or Color3.new(0, 0, 0)
     notify.Size = UDim2.new(1, 0, 0, 25)
     notify.BackgroundColor3 = color
+    notify.TextColor3 = textcolor
     notify.Text = text
     notify.TextSize = 20
     notify.Font = Enum.Font.Arimo
@@ -29,6 +32,10 @@ function notifier.new(text: string, duration: number, color: Color3)
             notify.Parent = nil
         end)
     end)
+end
+
+function notifier.delete()
+    gui:Destory()
 end
 
 return notifier
